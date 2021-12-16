96a3143ad687db908f019ff866c337ead9475461
Anton Malyugin <aamalyugin@avito.ru>
2021-12-16 19:30:56 +0300

    Added axios and added routes for page 404 and 500

--- src/express/express.js
+++ src/express/express.js
@@ -9,6 +9,8 @@ const articleRoutes = require(`./routes/article-routes`);
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 
+const {HTTP_STATUS_CODE} = require(`../constants`);
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
@@ -19,5 +21,12 @@ app.set(`view engine`, `pug`);
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articleRoutes);
+
+app.use((_, res) =>
+  res.status(HTTP_STATUS_CODE.BAD_REQUEST).render(`errors/404`)
+);
+app.use((_err, _req, res, _next) =>
+  res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).render(`errors/500`)
+);
 
 app.listen(DEFAULT_PORT);
