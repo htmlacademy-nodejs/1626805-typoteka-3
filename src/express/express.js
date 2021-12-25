5f9f08aa9bc92bf3beea82fdaba1e1e67c8d4179
Anton Malyugin <aamalyugin@avito.ru>
2021-12-25 18:21:16 +0300

    [module4-task3] - Refactored templates - Added api.js for fetching to the server

--- src/express/express.js
+++ src/express/express.js
@@ -2,9 +2,9 @@
 
 const express = require(`express`);
 const path = require(`path`);
-const mainRoutes = require(`./routes/main-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const articleRoutes = require(`./routes/article-routes`);
+const mainRoutes = require(`./routes/main/main.router`);
+const myRoutes = require(`./routes/my/my.router`);
+const articleRoutes = require(`./routes/articles/articles.router`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
@@ -13,20 +13,20 @@ const {HTTP_STATUS_CODE} = require(`../constants`);
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
-
-app.set(`views`, path.resolve(__dirname, `templates`));
-app.set(`view engine`, `pug`);
-
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articleRoutes);
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
 app.use((_, res) =>
-  res.status(HTTP_STATUS_CODE.BAD_REQUEST).render(`errors/404`)
+  res.status(HTTP_STATUS_CODE.BAD_REQUEST).render(`pages/errors/404`)
 );
 app.use((_err, _req, res, _next) =>
-  res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).render(`errors/500`)
+  res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).render(`pages/errors/500`)
 );
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
---
+++ src/express/routes/my/my.router.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const {Router} = require(`express`);
+const myRoute = new Router();
+
+myRoute.get(`/`, (req, res) => {
+  const content = {
+    articleList: [
+      {
+        id: 1,
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        title: `AirPods в один клик`,
+      },
+      {
+        id: 2,
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        title: `AirPods в один клик`,
+      },
+      {
+        id: 3,
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        title: `AirPods в один клик`,
+      },
+    ],
+  };
+
+  res.render(`pages/my/my`, content);
+});
+myRoute.get(`/comments`, (req, res) => {
+  const content = {
+    commentList: [
+      {
+        account: {
+          type: `user`,
+          avatar: `img/avatar-1.png`,
+          name: `Евгений Петров`,
+        },
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        text: `Автор, ты все выдумал, покайся`,
+        articleTitle: `AirPods в один клик`,
+      },
+      {
+        account: {
+          type: `user`,
+          avatar: `img/avatar-5.png`,
+          name: `Александр Марков`,
+        },
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        text: `Конечно, прежде чем так писать, нужно искренне приложить усилия, чтобы разобраться — не все люди умеют выражать свои мысли.`,
+        articleTitle: `AirPods в один клик`,
+      },
+      {
+        account: {
+          type: `user`,
+          avatar: `img/avatar-4.png`,
+          name: `Евгений Петров`,
+        },
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        text: `Автор, ты все выдумал, покайся`,
+        articleTitle: `AirPods в один клик`,
+      },
+      {
+        account: {
+          type: `user`,
+          avatar: `img/avatar-3.png`,
+          name: `Александр Марков`,
+        },
+        date: {
+          stamp: `2019-03-21T20:33`,
+          day: `21.03.2019`,
+          time: `20:33`,
+        },
+        text: `Конечно, прежде чем так писать, нужно искренне приложить усилия, чтобы разобраться — не все люди умеют выражать свои мысли.`,
+        articleTitle: `AirPods в один клик`,
+      },
+    ],
+  };
+
+  res.render(`pages/my/comments`, content);
+});
+
+module.exports = myRoute;
